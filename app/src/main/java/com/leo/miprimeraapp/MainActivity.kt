8759f14adc7f1b47e16423ca4abff0e4ac991e28
Leo Perez <leodanperez.1998@gmail.com>
2022-09-28 21:59:24 -0500

    Suebiendo cambios, se agrego la actividad SecondActivity

--- app/src/main/java/com/leo/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/leo/miprimeraapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.leo.miprimeraapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickButton()
+    }
+
+    private fun clickButton() {
+        val btnButton: Button = findViewById(R.id.btnButton)
+        val txtName: TextView = findViewById(R.id.txtName)
+        btnButton.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
