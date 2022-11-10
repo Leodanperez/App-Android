6cbcc83553622175eba7723e9644ef8544b4ccc3
Leo Perez <leodanperez.1998@gmail.com>
2022-11-09 20:34:37 -0500

    xd

--- app/src/main/java/com/leo/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/leo/miprimeraapp/MainActivity.kt
@@ -6,16 +6,31 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.leo.miprimeraapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        clickButton()
+        binding.btnLogin.setOnClickListener {
+            login()
+        }
     }
 
-    private fun clickButton() {
+    private fun login() {
+        val username = binding.txtUsername.editText?.text.toString()
+        val password = binding.txtPassword.editText?.text.toString()
+
+        val i = Intent(this, SecondActivity::class.java)
+        i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(i)
+        //Toast.makeText(this, "$username, $password", Toast.LENGTH_LONG).show()
 
     }
 }
