5119fe9fd201dc099a1e917dd11c6d395841c9f5
Leo Perez <leodanperez.1998@gmail.com>
2022-11-16 21:01:26 -0500

    se subieron cambios

--- app/src/main/java/com/leo/miprimeraapp/SecondActivity.kt
+++ app/src/main/java/com/leo/miprimeraapp/SecondActivity.kt
@@ -1,11 +1,56 @@
 package com.leo.miprimeraapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.leo.miprimeraapp.databinding.ActivitySecondBinding
+import java.io.File
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+    private var imageFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.circleImageUser.setOnClickListener {
+            selectImage()
+        }
+    }
+
+    private val startImageForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    val fileUri = data?.data
+                    imageFile = File(fileUri?.path)
+                    binding.circleImageUser.setImageURI(fileUri)
+                }
+                ImagePicker.RESULT_ERROR -> {
+                    Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_LONG).show()
+                }
+                else -> {
+                    Toast.makeText(this, "Task Cancel", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    private fun selectImage() {
+        ImagePicker.with(this)
+            .crop()
+            .compress(1024)
+            .maxResultSize(1080, 1080)
+            .createIntent { startImageForResult.launch(intent) }
     }
 }
